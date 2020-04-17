609f66f50eaeef732d1f55ee9407b0889c2b8668
Julian <Van Ost>
2020-04-17 15:38:01 -0700

    Updates

---
+++ precache-manifest.20e522cc31e0a744456b4371edd78776.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a9ace8e4fff2e0b60e52a8b6b0ebe3",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "ef3a01a9f29eae95eaab",
+    "url": "/portfolio-react/static/css/main.8ed62b97.chunk.css"
+  },
+  {
+    "revision": "d350f1527d884ab32256",
+    "url": "/portfolio-react/static/js/2.f792d811.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-react/static/js/2.f792d811.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef3a01a9f29eae95eaab",
+    "url": "/portfolio-react/static/js/main.9ef1b757.chunk.js"
+  },
+  {
+    "revision": "4c3bcdd375f26df9b5b7",
+    "url": "/portfolio-react/static/js/runtime-main.70e43ac6.js"
+  },
+  {
+    "revision": "e8ba8328ebaee74b2b6266fcc3c76c92",
+    "url": "/portfolio-react/static/media/AbutrackLogo.e8ba8328.png"
+  },
+  {
+    "revision": "e1c174aa101ce4dd182f1a12ab9685c7",
+    "url": "/portfolio-react/static/media/CartnerGolf.e1c174aa.png"
+  },
+  {
+    "revision": "3c3968d89383d7a6671c7e7fd3ede3de",
+    "url": "/portfolio-react/static/media/ci-logo.3c3968d8.png"
+  },
+  {
+    "revision": "0be9b6c2abe047cdcf180e7b97dd3877",
+    "url": "/portfolio-react/static/media/profilePicture.0be9b6c2.jpg"
+  }
+]);
