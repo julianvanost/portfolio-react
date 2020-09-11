8e8f3870f0b987ef6d1f2d8bfa82b914865f410a
Julian <Van Ost>
2020-09-10 17:07:50 -0700

    CS changes to links on navbar and alignement adjustments

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,19 +4,33 @@ import {
 } from 'react-router-dom'
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <span className="navbar-brand mb-1 h1">Julian Van Ost</span>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+      <span className="navbar-brand">Julian Van Ost</span>
+      <button className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarCollapse"
+        aria-controls="navbarCollapse"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <div className="navbar-nav">
-          <Link className="nav-item nav-link" to='/'>Home</Link>
-          <Link className="nav-item nav-link" to='/about'>About</Link>
-          <Link className="nav-item nav-link" to='/portfolio'>Portfolio</Link>
-          <Link className="nav-item nav-link" to='/contact'>Contact</Link>
-        </div>
+      <div className="collapse navbar-collapse" id="navbarCollapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <Link className="nav-item nav-link" to='/'>Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-item nav-link" to='/about'>About</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-item nav-link" to='/portfolio'>Portfolio</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-item nav-link" to='/contact'>Contact</Link>
+          </li>
+        </ul>
       </div>
     </nav>
 
