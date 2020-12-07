eb0f9816845f9455f59c58f7d85f30cb5625ae50
Julian <Van Ost>
2020-12-07 15:09:28 -0800

    Fixes to Home page image, text, and footer alignment

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -6,27 +6,25 @@ const Home = () => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-md-8">
-          <div className="page-header">
-            <h1>Home</h1>
+        <div className="page-header">
+          <h1>Home</h1>
+        </div>
+        <div class="row spiel">
+          <div class="col-md-3">
+            <img src={require("../../images/profilePicture.jpg")} alt="Julian Van Ost Profile" className="img-thumbnail"></img>
           </div>
-          <div className="row">
-            <div className="col-md-3">
-              <img src={require("../../images/profilePicture.jpg")} alt="Julian Van Ost Profile" className="img-thumbnail"></img>
-            </div>
-            <div className="col-md-9">
-              <p>
-                Greetings and Welcome to my new page! My name is Julian and I hope you enjoy the content on this site which includes some example projects, my resume, and a link to my certificate of completion for University of California, Irvine's Full-Stack Flex Web Development Bootcamp.
+          <div className="col-md-9">
+            <p>
+              Greetings and Welcome to my new page! My name is Julian and I hope you enjoy the content on this site which includes some example projects, my resume, and a link to my certificate of completion for University of California, Irvine's Full-Stack Flex Web Development Bootcamp.
               </p>
-              <p>
-                Below are some badges that I have earned while continuing my education in Web Development.
+            <p>
+              Below are some badges that I have earned while continuing my education in Web Development.
                 </p>
-            </div>
           </div>
         </div>
       </div>
       <footer>
-        <div className="row center">
+        <div className="row text-center">
           <div className='badges' data-iframe-width="150" data-iframe-height="270" data-share-badge-id="668da78b-3c2b-419b-b578-7d52714ef264" data-share-badge-host="https://www.youracclaim.com"></div>
           <div className='badges' data-iframe-width="150" data-iframe-height="270" data-share-badge-id="03b964e9-4e47-4771-a632-fe1925487d5d" data-share-badge-host="https://www.youracclaim.com"></div>
           <div className='badges' data-iframe-width="150" data-iframe-height="270" data-share-badge-id="ae88e3a9-de9a-465b-a44a-87ad57c3ca8e" data-share-badge-host="https://www.youracclaim.com"></div>
@@ -35,6 +33,13 @@ const Home = () => {
         </div>
       </footer>
     </div>
+    // <div class="row spiel">
+    //   <div class="col-md-3">
+    //     <img src="/portfolio-react/static/media/profilePicture.0be9b6c2.jpg" alt="Julian Van Ost Profile" class="img-thumbnail">
+    //       </div>
+    //       <div class="col-md-9">
+    //         <p>Greetings and Welcome to my new page! My name is Julian and I hope you enjoy the content on this site which includes some example projects, my resume, and a link to my certificate of completion for University of California, Irvine's Full-Stack Flex Web Development Bootcamp.</p><p>Below are some badges that I have earned while continuing my education in Web Development.</p></div></div>
+
   )
 }
 export default Home
