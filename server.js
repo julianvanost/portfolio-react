00e85450b67197f8a3d1cbf20d121d302463e26f
Julian <Van Ost>
2020-12-11 16:09:00 -0800

    Adds emailing support for contact form, just needs some configuration for the mail server

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-require('dotenv').config()
-const express = require('express')
-const { join } = require('path')
-const app = express();
-const port = process.env.PORT || 3001
-app.use(express.static(join(__dirname, 'client', 'build')))
-app.use(express.urlencoded({ extended: true }))
-app.get('/*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'))
-})
-app.listen(port, () => {
-   console.log('Server is up!')
-})
+// const path = require('path');
+// const express = require('express');
+// const app = express();
+
+// const buildPath = path.join(__dirname, '..', 'build');
+// app.use(express.json());
+// app.use(express.static(buildPath));
+
+// app.post('/send', (req, res) => {
+//   console.log(req.body);
+//   res.send(req.body);
+// });
+
+// app.listen(3030, () => {
+//   console.log('server start on port 3030');
+// });
+
+// require('dotenv').config()
+// const express = require('express')
+// const { join } = require('path')
+// const app = express();
+// app.use(express.static(join(__dirname, 'client', 'build')))
+// app.use(express.urlencoded({ extended: true }))
--- src/views/Contact/Contact.js
+++ src/views/Contact/Contact.js
@@ -1,20 +1,109 @@
-import React, { useState, useEffect } from 'react'
-import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Form, Button } from 'react-bootstrap';
 
-class Contact extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: "",
-      email: "",
-      message: "",
-      status: "Submit"
-    };
-  }
-  let buttonText = this.state.status;
+const Contact = () => {
+  const [state, setState] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  });
+
+  const [result, setResult] = useState(null);
+
+  const sendEmail = event => {
+    event.preventDefault();
+    axios
+      .post('/send', { ...state })
+      .then(response => {
+        setResult(response.data);
+        setState({
+          name: '',
+          email: '',
+          subject: '',
+          message: ''
+        });
+      })
+      .catch(() => {
+        setResult({
+          success: false,
+          message: 'Whoops! Something went wrong. Try again later.'
+        });
+      });
+  };
+
+  const onInputChange = event => {
+    const { name, value } = event.target;
+
+    setState({
+      ...state,
+      [name]: value
+    });
+  };
+
+  return (
+    <div>
+      {result && (
+        <p className={`${result.success ? 'success' : 'error'} text-center text-danger`}>
+          {result.message}
+        </p>
+      )}
+      <form onSubmit={sendEmail}>
+        <Form.Group controlId="name">
+          <Form.Label>Full Name</Form.Label>
+          <Form.Control
+            type="text"
+            name="name"
+            value={state.name}
+            placeholder="Enter your full name"
+            onChange={onInputChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="text"
+            name="email"
+            value={state.email}
+            placeholder="Enter your email"
+            onChange={onInputChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="subject">
+          <Form.Label>Subject</Form.Label>
+          <Form.Control
+            type="text"
+            name="subject"
+            value={state.subject}
+            placeholder="Enter subject"
+            onChange={onInputChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="subject">
+          <Form.Label>Message</Form.Label>
+          <Form.Control
+            as="textarea"
+            name="message"
+            value={state.message}
+            rows="3"
+            placeholder="Enter your message"
+            onChange={onInputChange}
+          />
+        </Form.Group>
+        <Button variant="primary" className="btn-submit" type="submit">
+          Submit
+        </Button>
+      </form>
+    </div>
+  )
 }
 
+export default Contact;
 
+
+
+// import React, { useState, useEffect } from 'react'
 
 // const Contact = () => {
 //   //must bring in useState in order to have a state in React hooks
@@ -40,66 +129,67 @@ class Contact extends Component {
 //   useEffect(() => {
 //   }, [countState.usernameInput])
 
-  return (
-    <main className="container mainStyle">
-      <div className="row">
-        <h1 className="heading col-12">
-          Contact
-        </h1>
+//   return (
+//     <main className="container mainStyle">
+//       <div className="row">
+//         <h1 className="heading col-12">
+//           Contact
+//         </h1>
+//         <div className="col-12">
+//           <form>
+//             <div className="row">
+//               <div className="form-group col">
+//                 <label htmlFor="formGroupExampleInput">Name</label>
+//                 <input
+//                   className="form-control"
+//                   id="formGroupExampleInput"
+//                   onChange={handleInputChange}
+//                   type="text"
+//                   name="usernameInput"
+//                   value={countState.usernameInput}
+//                   placeholder='Name'
+//                 ></input>
+//               </div>
+//               <div className="form-group col">
+//                 <label htmlFor="email">Email</label>
+//                 <input
+//                   className="form-control"
+//                   id="email"
+//                   onChange={handleInputChange}
+//                   type="text"
+//                   name="emailInput"
+//                   value={countState.emailInput}
+//                   placeholder="example@gmail.com"
+//                 ></input>
+//               </div>
+//             </div>
+//             <div className="row">
+//               <div className="form-group">
+//                 <label
+//                   htmlFor="message">Message</label>
+//                 <textarea
+//                   className="form-control"
+//                   onChange={handleInputChange}
+//                   id="message"
+//                   type='text'
+//                   name="messageInput"
+//                   value={countState.messageInput}
+//                   rows="3"
+//                   placeholder="Enter your message here"
+//                 ></textarea>
+//               </div>
+//               <br></br>
+//               <button className='btn btn-outline-primary btn-waves-effect float-right-custom'
+//                 onClick={handleSubmit}
+//               >Submit</button>
+//             </div>
+//           </form>
+//         </div>
+//       </div>
+//     </main>
+//   )
+// }
 
-        <div className="col-12">
-          <form>
-            <div className="row">
-              <div className="form-group col">
-                <label htmlFor="formGroupExampleInput">Name</label>
-                <input
-                  className="form-control"
-                  id="formGroupExampleInput"
-                  onChange={handleInputChange}
-                  type="text"
-                  name="usernameInput"
-                  value={countState.usernameInput}
-                  placeholder='Name'
-                ></input>
-              </div>
-              <div className="form-group col">
-                <label htmlFor="email">Email</label>
-                <input
-                  className="form-control"
-                  id="email"
-                  onChange={handleInputChange}
-                  type="text"
-                  name="emailInput"
-                  value={countState.emailInput}
-                  placeholder="example@gmail.com"
-                ></input>
-              </div>
-            </div>
-            <div className="row">
-              <div className="form-group">
-                <label
-                  htmlFor="message">Message</label>
-                <textarea
-                  className="form-control"
-                  onChange={handleInputChange}
-                  id="message"
-                  type='text'
-                  name="messageInput"
-                  value={countState.messageInput}
-                  rows="3"
-                  placeholder="Enter your message here"
-                ></textarea>
-              </div>
-              <br></br>
-              <button className='btn btn-outline-primary btn-waves-effect float-right-custom'
-                onClick={handleSubmit}
-              >Submit</button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </main>
-  )
-}
+// export default Contact
 
-export default Contact
+
