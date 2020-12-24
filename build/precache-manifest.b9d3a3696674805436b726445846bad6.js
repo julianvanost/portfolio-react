376782bfd2bc55659469105f4b0242ce55cc20c5
Julian <Van Ost>
2020-12-23 16:57:54 -0800

    Changes Badges to static images on Home view so that reload no longer breaks iframe badges

---
+++ build/precache-manifest.b9d3a3696674805436b726445846bad6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd886b7a8a3050306041b59094b32073",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "2a98dafa96588e70ef97",
+    "url": "/portfolio-react/static/css/main.e9244802.chunk.css"
+  },
+  {
+    "revision": "752434e3511d9c51ca9c",
+    "url": "/portfolio-react/static/js/2.f1e22829.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio-react/static/js/2.f1e22829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a98dafa96588e70ef97",
+    "url": "/portfolio-react/static/js/main.9d794687.chunk.js"
+  },
+  {
+    "revision": "2f26d0aed242c262185b",
+    "url": "/portfolio-react/static/js/runtime-main.a4de92d3.js"
+  },
+  {
+    "revision": "e8ba8328ebaee74b2b6266fcc3c76c92",
+    "url": "/portfolio-react/static/media/AbutrackLogo.e8ba8328.png"
+  },
+  {
+    "revision": "e1c174aa101ce4dd182f1a12ab9685c7",
+    "url": "/portfolio-react/static/media/CartnerGolf.e1c174aa.png"
+  },
+  {
+    "revision": "9eb4c7858d681ba567bd844d12ab0c37",
+    "url": "/portfolio-react/static/media/GraduationCertificate.9eb4c785.pdf"
+  },
+  {
+    "revision": "3c3968d89383d7a6671c7e7fd3ede3de",
+    "url": "/portfolio-react/static/media/ci-logo.3c3968d8.png"
+  },
+  {
+    "revision": "685fc9f53140f307161c03d2c6e3464f",
+    "url": "/portfolio-react/static/media/html-css.685fc9f5.png"
+  },
+  {
+    "revision": "7d1b19761967473615357304e1eff067",
+    "url": "/portfolio-react/static/media/javascript-and-jquery.7d1b1976.png"
+  },
+  {
+    "revision": "1ac9d5c90ed7a65b7d909cf61e93f799",
+    "url": "/portfolio-react/static/media/node-js-and-express.1ac9d5c9.png"
+  },
+  {
+    "revision": "0be9b6c2abe047cdcf180e7b97dd3877",
+    "url": "/portfolio-react/static/media/profilePicture.0be9b6c2.jpg"
+  },
+  {
+    "revision": "d6efbf4bf8ff15d4d8173b9eef63965c",
+    "url": "/portfolio-react/static/media/react-js.d6efbf4b.png"
+  },
+  {
+    "revision": "1f63bff2a171613f55ca9145f6e373e2",
+    "url": "/portfolio-react/static/media/sql-and-nosql-databases.1f63bff2.png"
+  }
+]);
--- src/views/Home/Badges.js
+++ src/views/Home/Badges.js
@@ -1,13 +1,21 @@
 import React from 'react'
+// import { Link } from 'react-router-dom'
 
 const Badges = () => {
   return (
     <div className="row text-center">
+      {/*
       <div className='badges' data-iframe-width="150" data-iframe-height="270" data-share-badge-id="668da78b-3c2b-419b-b578-7d52714ef264" data-share-badge-host="https://www.youracclaim.com"></div>
       <div className='badges' data-iframe-width="150" data-iframe-height="270" data-share-badge-id="03b964e9-4e47-4771-a632-fe1925487d5d" data-share-badge-host="https://www.youracclaim.com"></div>
       <div className='badges' data-iframe-width="150" data-iframe-height="270" data-share-badge-id="ae88e3a9-de9a-465b-a44a-87ad57c3ca8e" data-share-badge-host="https://www.youracclaim.com"></div>
       <div className='badges' data-iframe-width="150" data-iframe-height="270" data-share-badge-id="10424109-dd29-46b7-9b22-3eb169860e89" data-share-badge-host="https://www.youracclaim.com"></div>
       <div className='badges' data-iframe-width="150" data-iframe-height="270" data-share-badge-id="24f8f499-bfd6-4a74-8094-5552a9157340" data-share-badge-host="https://www.youracclaim.com"></div>
+      */}
+      <img className='badges' src={require("../../images/html-css.png")} alt='Badge - HTML and CSS'></img>
+      <img className='badges' src={require("../../images/javascript-and-jquery.png")} alt='Badge - JavaScript'></img>
+      <img className='badges' src={require("../../images/node-js-and-express.png")} alt='Badge - Node.js and Express'></img>
+      <img className='badges' src={require("../../images/react-js.png")} alt='Badge - React.js'></img>
+      <img className='badges' src={require("../../images/sql-and-nosql-databases.png")} alt='Badge - SQL and NoSQL Databases'></img>
     </div>
   )
 }
