6dc6d38970db147fd4789798d2f77dfab25ae2e8
Julian <Van Ost>
2020-12-26 16:47:28 -0800

    Navbar fix: nav closes on selection now; also, changes Brand link to navigate to Home and removes Home link

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import {
 const Navbar = props => {
   return (
     <nav className="sticky-top navbar navbar-expand-lg navbar-dark bg-dark mb-4">
-      <span className="navbar-brand">Julian Van Ost</span>
+      <Link className="navbar-brand" to="/home">Julian Van Ost</Link>
       <button className="navbar-toggler"
         type="button"
         data-toggle="collapse"
@@ -17,18 +17,27 @@ const Navbar = props => {
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarCollapse">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
+        <ul className="navbar-nav mr-auto" data-toggle="collapse" data-target="#navbarCollapse">
+          {/* <li className="nav-item">
             <Link className="nav-item nav-link" to='/'>Home</Link>
+          </li> */}
+          <li className="nav-item">
+            <Link
+              className="nav-item nav-link"
+              to='/about'
+            >About</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-item nav-link" to='/about'>About</Link>
+            <Link
+              className="nav-item nav-link"
+              to='/portfolio'
+            >Portfolio</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-item nav-link" to='/portfolio'>Portfolio</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-item nav-link" to='/contact'>Contact</Link>
+            <Link
+              className="nav-item nav-link"
+              to='/contact'
+            >Contact</Link>
           </li>
         </ul>
       </div>
